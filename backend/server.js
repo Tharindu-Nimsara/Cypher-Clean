376b9ae6553898fd4f4814707e5b8c1cc233a46f
Tharindu <tharinduonline1080@gmail.com>
2026-04-03 11:42:36 +0530

    new structure with basic middleware

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,12 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const fs = require("fs");
+const axios = require("axios");
+
 const app = express();
-const port = 3000;
+app.use(cors());
+app.use(express.json());
 
-// Basic GET route
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-// Start server
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
-});
+const PORT = 3001;
+app.listen(PORT, () => console.log("Backend running on port", PORT));
