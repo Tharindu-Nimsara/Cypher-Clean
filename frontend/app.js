20bb2f61acc26e76bc10c630db09826485a688c1
Anushka Rodrigo <anushka.rodrigo14@gmail.com>
2026-04-03 11:22:13 +0530

    imported elements from index.html to app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,8 @@
-//app.js file
+(() => {
+  const pickFolderBtn = document.getElementById("pickFolderBtn");
+  const scanBtn = document.getElementById("scanBtn");
+  const selectedPathText = document.getElementById("selectedPath");
+  const statusText = document.getElementById("status");
+  const loadingSpinner = document.getElementById("loadingSpinner");
+
+})();
